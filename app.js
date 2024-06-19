af064b3a2783c1d50e0e07b86d6e0ea58d00679a
Prince <principe3219@icloud.com>
2024-06-18 18:24:35 -0700

    light work

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 // for (let i = 0; i < array.length; i++) {}
 
-for (let i = 0; i < array.length; i++) {}
-for (let i = 0; i < array.length; i++) {}
-for (let i = 0; i < array.length; i++) {}
-for (let i = 0; i < array.length; i++) {}
-for (let i = 0; i < array.length; i++) {}
-for (let i = 0; i < array.length; i++) {}
+let p1 = 2
+let p2 = 4
+
+function myFunction(p1, p2) {
+    return p1 * p2;
+}
+
+console.log(myFunction)
+
+for (let i = 0; i < Array.length; i++) {}
